a9f92d8d86e1b0504f9c1b055ac28e39fe95ef44
Willian Silva <williansilva@alu.ufc.br>
2025-08-07 12:06:37 -0300

    Implement location retrieval and battery status for weather dashboard; enhance notifications for secure Wi-Fi connections

--- modules/dashboard.py
+++ modules/dashboard.py
@@ -1,23 +1,41 @@
-import requests, json, time
+import requests, json, time, termux, logging
+
+logging.basicConfig(
+    filename='dashboard.log', 
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s'
+    )
 
 def run_dashboard():
-    config  = json.load(open('config.json', 'r'))
-    city = config['city']
+    try:
+        city = termux.API.location()[1]
+        lat, lon = city['latitude'], city['longitude']
+       
+        city = requests.get(f'http://api.3geonames.org/{lat},{lon}.json', headers={'User-Agent': 'dashboardTermux'})
+        city.raise_for_status()
+       
+        city_data = city.json()
+        city = city_data['nearest']['city']
 
-    location = requests.get(
+    except Exception as e:
+        logging.error(f"Getting location from Termux API: {e}")
+        config = json.load(open('config.json', 'r'))
+        city = config['city']
+
+        location = requests.get(
         'https://nominatim.openstreetmap.org/search', 
         params={'q': city, 'format': 'json', 'limit': 1},
         headers={'User-Agent': 'dashboardTermux'}
         )
-
-    location.raise_for_status()
-
-    loc_data = location.json()
-    if not loc_data:
-        raise ValueError(f"City '{city}' not found in Nominatim database.")
         
-    lat, lon = loc_data[0]['lat'], loc_data[0]['lon']
-    time.sleep(1)
+        location.raise_for_status()
+
+        loc_data = location.json()
+        if not loc_data:
+            raise ValueError(f"City '{city}' not found in Nominatim database.")
+        
+        lat, lon = loc_data[0]['lat'], loc_data[0]['lon']
+        time.sleep(1)
 
     weather = requests.get(f'https://api.open-meteo.com/v1/forecast', params={
         'latitude': lat,
@@ -31,11 +49,31 @@ def run_dashboard():
     weather_data = weather.json()
     weather_info = weather_data['current']
 
-    print(f"Current weather in {city}:")
+    quote = requests.get('https://zenquotes.io/api/random', headers={'User-Agent': 'dashboardTermux'})
+    quote.raise_for_status()
+
+    quote_data = quote.json()[0]
+
+    print(f"Dashboard for {city} - {time.strftime('%Y-%m-%d %H:%M:%S')}")
+    print('-' * 20)
+
+    try:
+        battery = termux.API.battery()[1]
+        print(f"Battery level: {battery['level']}%")
+
+    except Exception as e:
+        logging.error(f"Getting battery status from Termux API: {e}")
+
     print(f"Temperature: {weather_info['temperature_2m']}°C")
     print(f"Humidity: {weather_info['relative_humidity_2m']}%")
     print(f"Is it day? {'Yes' if weather_info['is_day'] == 1 else 'No'}")
     print(f"Rain: {weather_info['rain']} mm")
+
+    print('-' * 20)
+
+    print("Quote of the day:")
+
+    print(f"{quote_data['q']} - {quote_data['a']}")
 
 if __name__ == "__main__":
     run_dashboard()
--- modules/network_monitor.py
+++ modules/network_monitor.py
@@ -1,5 +1,23 @@
+import json, termux
+
 def run_wifi_monitor():
-    pass
+    wifi = termux.Wifi.info()[1]
+
+    secure_networks = json.load(open("config.json"))["secure_networks"]
+
+    for network in secure_networks:
+        if wifi['ssid'] == network:
+            termux.Notification.notify(
+                title="Secure Network Connected",
+                content=f"You are connected to a secure network: {network['ssid']}"
+            )
+            return
+        
+    termux.Notification.notify(
+        title="No Secure Network",
+        content="You are not connected to any secure networks."
+    )
+        
 
 if __name__ == "__main__":
     run_wifi_monitor()
